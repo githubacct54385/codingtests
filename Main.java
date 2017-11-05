8c15d000d42d9328f37f00048f414a63ea5aa5bd
githubacct54385 <alexbarke002@gmail.com>
2017-11-05 17:05:35 +0530

    Added Dijkstra's algorithm

--- Main.java
+++ Main.java
@@ -11,7 +11,11 @@ public class Main {
         System.out.println(CodilityExamQ1("09:42", "11:39"));
 
         System.out.println("Binary Gap: ");
-        System.out.println(BinaryGap(9));
+        System.out.println(BinaryGap(1041));
+
+        System.out.println("Odd Occurrences in Array");
+        int[] oddOccurrencesArr = {9, 3, 9, 3, 9, 7, 9};
+        System.out.println(OddOccurencesInArray(oddOccurrencesArr));
 
         System.out.println("Cyclic Rotation: ");
         int CyclicRotationArr[] = {3, 8, 9, 7, 6};
@@ -73,6 +77,19 @@ public class Main {
         int stoneWallArr[] = {8, 8, 5, 7, 9, 8, 7, 4, 8};
         System.out.println(StoneWall(stoneWallArr, stoneWallArr.length));
 
+        System.out.println("Dijkstra Example");
+        int graph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        };
+        dijkstra(graph, 0);
+
 
         //=============================================
         // Additional In Progress Coding Example Tests
@@ -96,6 +113,93 @@ public class Main {
         //int A[] = {1, 50, 50, 50, 1};
         //System.out.println(solution2(A));
 
+    }
+
+
+    // Dijkstra Solution from
+    // http://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
+    private static final int V = 9;
+    private static void PrintDijkstraSolution(int dist[], int n) {
+        System.out.println("Vertex   Distance from Source");
+        for (int i = 0; i < V; i++)
+            System.out.println(i+" tt "+dist[i]);
+    }
+
+    private static int MinDistance(int dist[], Boolean sptSet[])
+    {
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index=-1;
+
+        for (int v = 0; v < V; v++)
+            if (!sptSet[v] && dist[v] <= min)
+            {
+                min = dist[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
+
+    // Funtion that implements Dijkstra's single source shortest path
+    // algorithm for a graph represented using adjacency matrix
+    // representation
+    static void dijkstra(int graph[][], int src)
+    {
+        int dist[] = new int[V]; // The output array. dist[i] will hold
+        // the shortest distance from src to i
+
+        // sptSet[i] will true if vertex i is included in shortest
+        // path tree or shortest distance from src to i is finalized
+        Boolean sptSet[] = new Boolean[V];
+
+        // Initialize all distances as INFINITE and stpSet[] as false
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
+
+        // Distance of source vertex from itself is always 0
+        dist[src] = 0;
+
+        // Find shortest path for all vertices
+        for (int count = 0; count < V-1; count++)
+        {
+            // Pick the minimum distance vertex from the set of vertices
+            // not yet processed. u is always equal to src in first
+            // iteration.
+            int u = MinDistance(dist, sptSet);
+
+            // Mark the picked vertex as processed
+            sptSet[u] = true;
+
+            // Update dist value of the adjacent vertices of the
+            // picked vertex.
+            for (int v = 0; v < V; v++)
+
+                // Update dist[v] only if is not in sptSet, there is an
+                // edge from u to v, and total weight of path from src to
+                // v through u is smaller than current value of dist[v]
+                if (!sptSet[v] && graph[u][v]!=0 &&
+                        dist[u] != Integer.MAX_VALUE &&
+                        dist[u]+graph[u][v] < dist[v])
+                    dist[v] = dist[u] + graph[u][v];
+        }
+
+        // print the constructed distance array
+        PrintDijkstraSolution(dist, V);
+    }
+
+
+
+
+
+    private static int OddOccurencesInArray(int[] A) {
+        int result = 0;
+        for (int aA : A) {
+            result ^= aA;
+        }
+        return result;
     }
 
     private static int StoneWall(int H[], int N) {
