2c8df94008125a183d1b451b798c3be9f3ad3f66
githubacct54385 <alexbarke002@gmail.com>
2017-11-07 10:18:39 +0530

    Added Breadth First Search (BFS) implementation

--- Main.java
+++ Main.java
@@ -90,6 +90,21 @@ public class Main {
         };
         dijkstra(graph, 0);
 
+        System.out.println("\n\nBreadth First Traversal");
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal "+
+                "(starting from vertex 2)");
+
+        g.BFS(2);
+
 
         //=============================================
         // Additional In Progress Coding Example Tests
@@ -113,6 +128,58 @@ public class Main {
         //int A[] = {1, 50, 50, 50, 1};
         //System.out.println(solution2(A));
 
+    }
+
+    static class Graph {
+        private int V;   // No. of vertices
+        private LinkedList[] adj; //Adjacency Lists
+
+        // Constructor
+        Graph(int v)
+        {
+            V = v;
+            adj = new LinkedList[v];
+            for (int i=0; i<v; ++i)
+                adj[i] = new LinkedList();
+        }
+
+        // Function to add an edge into the graph
+        void addEdge(int v,int w)
+        {
+            adj[v].add(w);
+        }
+
+        // prints BFS traversal from a given source s
+        void BFS(int s)
+        {
+            // Mark all the vertices as not visited(By default
+            // set as false)
+            boolean visited[] = new boolean[V];
+
+            // Create a queue for BFS
+            LinkedList<Integer> queue = new LinkedList<Integer>();
+
+            // Mark the current node as visited and enqueue it
+            visited[s]=true;
+            queue.add(s);
+
+            while (queue.size() != 0)
+            {
+                // Dequeue a vertex from queue and print it
+                s = queue.poll();
+                System.out.print(s+" ");
+
+                // Get all adjacent vertices of the dequeued vertex s
+                // If a adjacent has not been visited, then mark it
+                // visited and enqueue it
+                for (Integer n : (Iterable<Integer>) adj[s]) {
+                    if (!visited[n]) {
+                        visited[n] = true;
+                        queue.add(n);
+                    }
+                }
+            }
+        }
     }
 
 
